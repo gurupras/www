196f45a884f4859b1e9f1aacb26c6ab057a9d71d
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2016-01-26 15:19:27 -0500

    Added custom deck plugin for deck header
    This adds an login/logout and a sync button to each slide

---
+++ src/assets/js/slides/deck/15_deck.header.js
@@ -0,0 +1,73 @@
+/*!
+Deck JS - deck.header
+Copyright (c) 2011-2014 Caleb Troughton
+Dual licensed under the MIT license.
+https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
+*/
+
+/*
+This module adds the necessary methods and key bindings to show and hide a form
+for jumping to any slide number/id in the deck (and processes that form
+accordingly). The form-showing state is indicated by the presence of a class on
+the deck container.
+*/
+(function($, undefined) {
+  var $document = $(document);
+  var rootCounter;
+
+  var maybeAddSnippet = function() {
+    $('<input/>').addClass('btn-login').attr('type', 'submit').attr('value', 'Login').css('float', 'right').attr('id', 'btn-login')
+    .on('click', function(e) {
+      e.preventDefault();
+      auth0Lock.show(function(err, profile, token) {
+        if(err) {
+          alert('There was an error');
+        } else {
+          localStorage.setItem('userToken', token);
+          userProfile = profile;
+          $('#btn-login').hide();
+          $('#btn-logout').show();
+        }
+      });
+    })
+    .appendTo($.deck('getContainer'));
+
+    $('<input/>').addClass('btn-login').attr('type', 'submit').attr('value', 'Logout').attr('id', 'btn-logout')
+    .css('float', 'right')
+    .css('display', 'none')
+    .on('click', function() {
+      localStorage.removeItem('userToken');
+      userProfile = null;
+      $('#btn-logout').hide();
+      $('#btn-login').show();
+    });
+
+    $('<button/>').addClass('').attr('id', 'btn-sync').css('float', 'right').html('Sync')
+    .on('click', function(e) {
+      if($('#btn-sync').html() === 'Sync') {
+        shouldSync = true;
+        $('#btn-sync').prop('disabled', true);
+      } else {
+      }
+    })
+    .appendTo($.deck('getContainer'));
+  };
+
+
+  var bindKeyEvents = function() {
+  };
+
+  var populateDatalist = function() {
+  };
+
+  var markRootSlides = function() {
+  };
+
+  var handleFormSubmit = function() {
+  };
+
+  $document.bind('deck.init', function() {
+    maybeAddSnippet();
+  });
+})(jQuery);
+
